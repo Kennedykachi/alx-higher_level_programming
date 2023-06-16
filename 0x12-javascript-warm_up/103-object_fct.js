a0cb9f2f53e458c6bbdbcf84e0bb930f82fb6804
Kennedykachi <kennedyonyekachi@gmail.com>
2023-06-15 17:31:41 -0700

    increment object

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,9 +4,13 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
+/*
+YOUR CODE HERE
+*/
 myObject.incr = function () {
   this.value++;
 };
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
