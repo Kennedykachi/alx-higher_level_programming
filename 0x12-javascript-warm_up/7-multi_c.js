485200bfc925f06acfcef260c49dbc9c2dce0d1c
Kennedykachi <kennedyonyekachi@gmail.com>
2023-06-15 17:19:46 -0700

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-  console.log('Missing number of occurrences');
-} else {
-  const x = Number(process.argv[2]);
-  let i = 0;
-  while (i < x) {
-    console.log('C is fun');
-    i++;
-  }
+let num = process.argv[2];
+num = Number(num);
+
+for (let i = 0; i < num; i++) {
+  console.log('C is fun');
 }
