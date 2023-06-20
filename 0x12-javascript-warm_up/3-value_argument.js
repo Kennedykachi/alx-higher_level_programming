a12144a95017bdadfa36608fb640d40eaf0ab249
Kennedykachi <kennedyonyekachi@gmail.com>
2023-06-19 22:06:33 -0700

    making executable

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-const numArgs = process.argv;
-
-if (numArgs[2] === undefined) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
-  console.log(numArgs[2]);
-}}
+  console.log(process.argv[2]);
+}
