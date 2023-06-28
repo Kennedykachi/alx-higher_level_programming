115b55717e5b66de5168d78c5d1a8f2ed78499b5
Kennedykachi <kennedyonyekachi@gmail.com>
2023-06-28 06:46:50 -0700

    making files executable

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Square;#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Square;
